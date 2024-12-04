6eecaa93b99afd5546ff5fe7a20dc857787dae6e
Neil Richardson <neil.richardson@tapeark.com>
2024-12-04 09:26:39 +0000

    Day 3

---
+++ day03/part02/main.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+        "fmt"
+        "os"
+        "regexp"
+        "strconv"
+        "strings"
+)
+
+func main() {
+        filename := os.Args[1]
+
+        data, err := os.ReadFile(filename)
+        if err != nil {
+            panic(err)
+        }
+
+        dataset := strings.TrimSuffix(string(data), "\n")
+        r, _ := regexp.Compile("(mul\\(\\d{1,3},\\d{1,3}\\)|do\\(\\)|don't\\(\\))")
+        match := r.FindAllString(dataset, -1)
+
+        enabled := true
+        var sum int64
+        r2, _ := regexp.Compile("\\d{1,3}")
+        for _, v := range match {
+            if v == "do()" {
+                enabled = true
+                continue
+            } else if v == "don't()" {
+                enabled = false
+                continue
+            }
+
+            if !enabled {
+                continue
+            }
+
+            num_match := r2.FindAllString(v, -1)
+
+            if len(num_match) != 2 {
+                panic("Expected 2 numbers")
+            }
+
+            a, err := strconv.ParseInt(num_match[0], 10, 64)
+            if err != nil {
+                panic(err)
+            }
+
+            b, err := strconv.ParseInt(num_match[1], 10, 64)
+            if err != nil {
+                panic(err)
+            }
+
+            sum += a * b
+        }
+
+        fmt.Println(sum)
+}
